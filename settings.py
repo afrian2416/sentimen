43f604278dda8552fba67087bbb48ecabd5d80d1
afrian2416 <afrian.r12@gmail.com>
2024-06-11 15:55:17 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import streamlit as st
 def settings():
     st.title("Settings")
     st.subheader("User Settings")
+    Name = st.text_input("Name", value="Name")
     username = st.text_input("Username", value="User")
     password = st.text_input("Password", type="password", value="Password")
     if st.button("Update Settings"):
