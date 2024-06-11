7bf0228ef504b82f537171c6279f46f4633b4847
afrian2416 <afrian.r12@gmail.com>
2024-06-11 15:48:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
     with st.sidebar:
         selected = option_menu("Sentiment Analysis", list(PAGES.keys()), 
                                icons=['house', 'bar-chart-line', 'clock-history', 'gear'], 
-                               menu_icon="", default_index=0)
+                                default_index=0)
         
     page = PAGES[selected]
     page()
